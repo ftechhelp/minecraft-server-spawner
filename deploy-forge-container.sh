6c9537616d66027d2cafe6d9bc902b7f5f48bd2f
Vincent Fontaine <ftechhelp@gmail.com>
2024-07-15 20:39:29 -0700

    found right container for project

--- deploy-forge-container.sh
+++ deploy-forge-container.sh
@@ -12,7 +12,8 @@ print_help() {
     echo "  --minecraftVersion VERSION   Specify Minecraft version"
     echo "  --forgeVersion VERSION       Specify Forge version"
     echo "  --port NUMBER                Specify port number"
-    echo "  --help                       Display this help message"
+    echo "  --name STRING                Specify the name of the container"
+    echo "  --help                       Show this help message"
     echo ""
     echo "Example:"
     echo "  $0 --forgeVersion 40.2.0 --minecraftVersion 1.18.2 --port 5000"
@@ -24,6 +25,7 @@ while [[ "$#" -gt 0 ]]; do
         --minecraftVersion) MINECRAFT_VERSION="$2"; shift ;;
         --forgeVersion) FORGE_VERSION="$2"; shift ;;
         --port) PORT="$2"; shift ;;
+        --name) NAME="$2"; shift ;;
         --help) print_help; exit 0 ;;
         *) echo "Unknown parameter: $1"; print_help; exit 1 ;;
     esac
@@ -31,7 +33,7 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 # Check if required parameters are provided
-if [ -z "$MINECRAFT_VERSION" ] || [ -z "$FORGE_VERSION" ] || [ -z "$PORT" ]; then
+if [ -z "$MINECRAFT_VERSION" ] || [ -z "$FORGE_VERSION" ] || [ -z "$PORT" ] || [ -z "$NAME" ]; then
     echo "Error: Missing required parameters."
     print_help
     exit 1
@@ -41,15 +43,17 @@ fi
 echo "Building Forge container with the following parameters:"
 echo "Minecraft Version: $MINECRAFT_VERSION"
 echo "Forge Version: $FORGE_VERSION"
+echo "Name: $NAME"
 echo "Port: $PORT"
 
-docker build \
-    --build-arg MINECRAFT_VERSION=$MINECRAFT_VERSION \
-    --build-arg FORGE_VERSION=$FORGE_VERSION \
-    --build-arg PORT=$PORT \
-    -t forge-server .
-
 # Run the container
-docker run -d -p $PORT:25565 --name forge-server forge-server
+docker run \
+    -d -it -p $PORT:25565 \
+    -e EULA=TRUE \
+    -e TYPE=FORGE \
+    -e VERSION=$MINECRAFT_VERSION \
+    -e FORGE_VERSION=$FORGE_VERSION \
+    --name forge-server-$PORT-$MINECRAFT_VERSION-$FORGE_VERSION-$NAME \
+    itzg/minecraft-server
 
 echo "Forge container built and launched successfully."
