339528ef384a768c53699dc65fc017271fe8b975
Vincent Fontaine <ftechhelp@gmail.com>
2024-07-22 22:18:58 -0700

    spawner works

--- DockerCompose.py
+++ DockerCompose.py
@@ -1,13 +1,23 @@
 import yaml
+from python_on_whales import docker
+import uuid
+import os
 
-class DockerCompose:
+class Spawner:
     
-    def __init__(self, file_path):
-        self.file_path = file_path
+    def __init__(self, file_name):
+        self.file_name = file_name
 
-    def modify_docker_compose(self, name: str = None, new_port: int = None, new_volume: str = None, new_type: str = None, new_minecraftVersion: str = None, new_forgeVersion: str = None, mods: list = None):
+    def create_or_modify_spawn(self, name: str = None, new_port: int = None, new_volume: str = None, new_type: str = None, new_minecraftVersion: str = None, new_forgeVersion: str = None, mods: list = None, server_properties: list = None):
         
-        with open(self.file_path, 'w+') as file:
+        spawn_name = name or str(uuid.uuid4())
+        spawn_directory = f"./spawns/{spawn_name}"
+        docker_compose_file_path = f"{spawn_directory}/{self.file_name}"
+
+        if not os.path.exists(spawn_directory):
+            os.makedirs(spawn_directory)
+
+        with open(docker_compose_file_path, 'w+') as file:
             docker_compose = yaml.safe_load(file) or {}
 
         if 'services' not in docker_compose:
@@ -15,9 +25,11 @@ class DockerCompose:
         if 'mc' not in docker_compose['services']:
             docker_compose['services']['mc'] = {}
 
-        docker_compose['services']['mc']['container_name'] = f'{name or "default"}-{new_port or 25565}-{new_minecraftVersion or "LATEST"}-{new_forgeVersion or "LATEST"}-{new_type or "FORGE"}'
+        container_name = f'{spawn_name}-{new_port or 25565}-{new_minecraftVersion or "LATEST"}-{new_forgeVersion or "LATEST"}-{new_type or "FORGE"}'
+
+        docker_compose['services']['mc']['container_name'] = container_name
         docker_compose['services']['mc']['ports'] = [f"{new_port or 25565}:25565"]
-        docker_compose['services']['mc']['volumes'] = [f"{new_volume or "./data"}:/data"]
+        docker_compose['services']['mc']['volumes'] = [f"{new_volume or f"./data"}:/data"]
         docker_compose['services']['mc']['image'] = "itzg/minecraft-server"
         docker_compose['services']['mc']['stdin_open'] = True
         docker_compose['services']['mc']['tty'] = True
@@ -27,22 +39,26 @@ class DockerCompose:
         docker_compose['services']['mc']['environment'] += [f"FORGE_VERSION={new_forgeVersion or "LATEST"}"]
         docker_compose['services']['mc']['environment'] += ["CF_API_KEY=${CF_API_KEY}"] # Make sure you have a .env file with the CF_API_KEY variable
         docker_compose['services']['mc']['environment'] += ["EULA=TRUE"]
+        docker_compose['services']['mc']['environment'] += ["REMOVE_OLD_MODS=TRUE"]
+        docker_compose['services']['mc']['environment'] += ["INIT_MEMORY=1G"]
+        docker_compose['services']['mc']['environment'] += ["MAX_MEMORY=4G"]
+
+        for property in server_properties or []:
+            docker_compose['services']['mc']['environment'] += [f"{property}"]
+        
         mods_list = mods or []
         if (mods_list):
             docker_compose['services']['mc']['environment'] += [f"CURSEFORGE_FILES={" ".join(mods_list)}"]
 
 
         # Write the modified configuration back to the file
-        with open(self.file_path, 'w') as file:
+        with open(docker_compose_file_path, 'w') as file:
             yaml.dump(docker_compose, file, default_flow_style=False)
 
         print("Docker Compose file updated successfully.")
-        
-        self.__run_docker_compose()
 
-    def __run_docker_compose(self):
-        import subprocess
-        subprocess.run(f'docker compose up --build --force-recreate --no-deps', shell=True)
+        os.chdir(spawn_directory)
+        docker.compose.up(detach=True, force_recreate=True, recreate=True, attach_dependencies=False, build=True)
 
-dc = DockerCompose('docker-compose.yml')
-dc.modify_docker_compose(new_minecraftVersion="1.20.1", mods=['https://www.curseforge.com/minecraft/mc-mods/dragon-ball-mod-1-16-5/files/5541977'])
+dc = Spawner('docker-compose.yml')
+dc.create_or_modify(name="268f1896-dcd3-44c9-9864-e6c3bdc63728", new_minecraftVersion="1.18.2", mods=['https://www.curseforge.com/minecraft/mc-mods/jei'], new_port=400, server_properties=['LEVEL_NAME=Vince Server', 'VIEW_DISTANCE=10', 'DIFFICULTY=normal'])
