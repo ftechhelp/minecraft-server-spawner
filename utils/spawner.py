5c082335fdafc9f06bcb9873ef7efd3fe6c860f0
Vincent Fontaine <ftechhelp@gmail.com>
2024-07-23 22:15:04 -0700

    added spawn class

--- utils/spawner.py
+++ utils/spawner.py
@@ -1,74 +1,51 @@
-import yaml
-from python_on_whales import docker
 import uuid
-import os
-import shutil
+from models.spawn import Spawn
 
 class Spawner:
     
-    def __init__(self, file_name):
-        self.file_name = file_name
+    def __init__(self):
+        self.spawns: list = []
 
-    def create_or_modify_spawn(self, name: str = None, new_port: int = None, new_volume: str = None, new_type: str = None, new_minecraftVersion: str = None, new_forgeVersion: str = None, mods: list = None, server_properties: list = None):
-        
-        spawn_name = name or str(uuid.uuid4())
-        spawn_directory = f"./spawns/{spawn_name}"
-        docker_compose_file_path = f"{spawn_directory}/{self.file_name}"
-
-        if not os.path.exists(spawn_directory):
-            os.makedirs(spawn_directory)
-
-        with open(docker_compose_file_path, 'w+') as file:
-            docker_compose = yaml.safe_load(file) or {}
+    def create_or_modify_spawn(self, name: str = str(uuid.uuid4()), new_port: int = 25565, new_volume: str = "./data", new_type: str = "FORGE", new_minecraftVersion: str = "LATEST", new_forgeVersion: str = "LATEST", mods: list = [], server_properties: list = []) -> None:
+        spawn = Spawn(name, new_port, new_volume, new_type, new_minecraftVersion, new_forgeVersion, mods, server_properties)
+        docker_compose = spawn.get_docker_compose_contents()
 
         if 'services' not in docker_compose:
             docker_compose['services'] = {}
         if 'mc' not in docker_compose['services']:
             docker_compose['services']['mc'] = {}
 
-        docker_compose['services']['mc']['container_name'] = spawn_name
-        docker_compose['services']['mc']['ports'] = [f"{new_port or 25565}:25565"]
-        docker_compose['services']['mc']['volumes'] = [f"{new_volume or f"./data"}:/data"]
+        docker_compose['services']['mc']['container_name'] = spawn.name
+        docker_compose['services']['mc']['ports'] = [f"{spawn.port}:25565"]
+        docker_compose['services']['mc']['volumes'] = [f"{spawn.volume}:/data"]
         docker_compose['services']['mc']['image'] = "itzg/minecraft-server"
         docker_compose['services']['mc']['stdin_open'] = True
         docker_compose['services']['mc']['tty'] = True
 
-        docker_compose['services']['mc']['environment'] = [f"TYPE={new_type or "FORGE"}"]
-        docker_compose['services']['mc']['environment'] += [f"VERSION={new_minecraftVersion or "LATEST"}"]
-        docker_compose['services']['mc']['environment'] += [f"FORGE_VERSION={new_forgeVersion or "LATEST"}"]
+        docker_compose['services']['mc']['environment'] = [f"TYPE={spawn.type}"]
+        docker_compose['services']['mc']['environment'] += [f"VERSION={spawn.minecraft_version}"]
+        docker_compose['services']['mc']['environment'] += [f"FORGE_VERSION={spawn.forge_version}"]
         docker_compose['services']['mc']['environment'] += ["CF_API_KEY=${CF_API_KEY}"] # Make sure you have a .env file with the CF_API_KEY variable
         docker_compose['services']['mc']['environment'] += ["EULA=TRUE"]
         docker_compose['services']['mc']['environment'] += ["REMOVE_OLD_MODS=TRUE"]
         docker_compose['services']['mc']['environment'] += ["INIT_MEMORY=1G"]
         docker_compose['services']['mc']['environment'] += ["MAX_MEMORY=4G"]
 
-        for property in server_properties or []:
+        for property in spawn.server_properties:
             docker_compose['services']['mc']['environment'] += [f"{property}"]
         
-        mods_list = mods or []
-        if (mods_list):
-            docker_compose['services']['mc']['environment'] += [f"CURSEFORGE_FILES={" ".join(mods_list)}"]
+        if (spawn.mods):
+            docker_compose['services']['mc']['environment'] += [f"CURSEFORGE_FILES={" ".join(spawn.mods)}"]
 
-
-        # Write the modified configuration back to the file
-        with open(docker_compose_file_path, 'w') as file:
-            yaml.dump(docker_compose, file, default_flow_style=False)
-
+        spawn.set_docker_compose_contents(docker_compose)
         print("Docker Compose file updated successfully.")
 
-        os.chdir(spawn_directory)
-        docker.compose.up(detach=True, force_recreate=True, recreate=True, attach_dependencies=False, build=True)
+        spawn.up()
+        self.spawns.append(spawn)
 
     def purge_spawn(self, name: str):
-
-        os.chdir(f"./spawns/{name}")
-
-        docker.compose.down(remove_images="all", volumes=True, remove_orphans=True)
-
-        spawn_directory = f"../{name}"
-
-        if os.path.exists(spawn_directory):
-            shutil.rmtree(spawn_directory)
-            print("spawn directory purged.")
-        else:
-            print("No such spawn exists.")
+        for spawn in self.spawns:
+            if spawn.name == name:
+                spawn.purge()
+                self.spawns.remove(spawn)
+                break
